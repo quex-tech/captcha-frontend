fc87a442694936d8429915429cc2b7cf115659d7
Stanislav Zharkov <zharkovstas@gmail.com>
2025-07-28 13:05:03 +0400

    Updates

---
+++ assets/transactions-D_JpUCqG.js
@@ -0,0 +1,16 @@
+import{i as f,a,x as m}from"./index-DVLvrzlE.js";import{c as p}from"./if-defined-CMMef0Zg.js";import"./index-C8iLC-vX.js";import"./index-B6jEfW0K.js";import"./index-CV2kiFc1.js";import"./index-BXdp7Prj.js";import"./index-Dba-GxoL.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
